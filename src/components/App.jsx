1a469a67bb44dfb0e71d417bd0cc8d3bc1d06e7c
BogdanMU <104568558+BogdanMU@users.noreply.github.com>
2022-12-16 13:48:14 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,6 @@ import { ContactsFilter } from './ContactsFilter/ContactsFilter';
 import { ContactForm } from './ContactsForm/ContactForm';
 import { Wrap } from './App.styled';
 
-const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
 export class App extends Component {
   state = {
     contacts: null,
